df0bd7c75c604d1a6bc58e3c6d8bf7ceaaf6ab2a
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-06-17 15:47:16 -0300

    requirement 4 done, teardown jest

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,6 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
   api.fetchURL.mockImplementation(async () => {
     try {
       return {
