5468f93aab0ed46d927a72e436bbe24dd849eb53
Kevin Oliveira <kevin.zero@hotmail.com>
2021-06-17 15:47:51 -0300

    (4) - Cria funcoes no arquivo test/setupTeardown.spec.js

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,6 +21,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
+  beforeEach(() => adventure.randomAttack());
+  afterEach(() => console.log(adventure.specialists));
+  afterAll(() => console.log(`O sobrevivente foi: ${adventure.specialists[0].nome}`));
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
