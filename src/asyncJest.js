4d48f5b99373583e172d5b40dbbebc6efae2af4c
ArturAlvaro <artur.alvaro73@gmail.com>
2021-06-17 15:49:10 -0300

    Requisito 1

--- src/asyncJest.js
+++ src/asyncJest.js
@@ -2,7 +2,10 @@
 const answerPhone = (answer) =>
   new Promise(function (resolve, reject) {
     setTimeout(
-      answer ? resolve('Oi!') : reject(new Error('Infelizmente não podemos atender...')), 3000,
+      answer
+        ? resolve('Oi!')
+        : reject(new Error('Infelizmente não podemos atender...')),
+      3000,
     );
   });
 
--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,5 @@
-const assert = require('assert');
-// const answerPhone = require('../src/asyncJest');
+// const assert = require('assert');
+const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -11,12 +11,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
+  test('atende', async () => {
     // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    await expect(answerPhone(true)).resolves.toBe('Oi!');
   });
-  test('ocupado', () => {
-    assert.fail();
+  test('ocupado', async () => {
     // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    await expect(answerPhone(false)).rejects.toThrowError(
+      'Infelizmente não podemos atender...',
+    );
   });
 });
