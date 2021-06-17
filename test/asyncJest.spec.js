f864c45cffc39d64587abc6191d9dc618e0af7e4
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-06-17 15:10:37 -0300

    Realiza requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,3 +1,4 @@
+const answerPhone = require('../src/asyncJest');
 // const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -10,12 +11,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('atende', async () => {
+    await expect(answerPhone(true)).resolves.toBe('Oi!');
   });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado', async () => {
+    const answer = new Error('Infelizmente não podemos atender...');
+    await expect(answerPhone(false)).rejects.toEqual(answer);
   });
 });
