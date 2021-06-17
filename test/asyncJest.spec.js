5193fa5140449716345f8db640675c0e8e226a71
Alexander <alexanderteodorocastro95@hotmail.com>
2021-06-17 15:04:48 -0300

    Inciando Projeto

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
     // Insira seu teste assíncrono aqui
   });
   test('ocupado', () => {
-    assert.fail();
     // Insira seu teste assíncrono aqui
   });
 });
