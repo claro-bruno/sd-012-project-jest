cbb8d87de441cf5c3e4d05d037e41b4e42fc8c64
Marcela Silva <marcela.ops@hotmail.br>
2021-06-17 17:04:58 -0300

    requisito 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,6 +21,8 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
+  beforeEach(() => adventure.randomAttack());
+  afterAll(() => console.log(`O aventureiro que sobreviveu foi ${adventure.specialists[0].nome}.`));
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
