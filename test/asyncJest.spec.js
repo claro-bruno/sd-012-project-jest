decb4805aa83dc727aedebc0217d479a3f8808fe
isaacbatst <isaacbatst@gmail.com>
2021-06-17 11:50:52 -0300

    test project

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 // const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -12,8 +11,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+
   });
   test('ocupado', () => {
     assert.fail();
