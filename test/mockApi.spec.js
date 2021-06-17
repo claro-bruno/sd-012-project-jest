e465e2c7318a7f5f5f03370f5d9736ade7f18442
PeDD1 <pedrohr0909@gmail.com>
2021-06-17 18:41:06 -0300

    Requisito 1 refatorado e 2 concluido com lint

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -21,9 +21,25 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
+jest.mock('../src/mockApi');
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  api.fetchURL.mockImplementation(async () => ({
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  }));
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
