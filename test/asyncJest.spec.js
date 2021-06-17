01c295a12eb20e2e6040b377a7382118f222a075
Henrique Remedios <henriqueremedios@gmail.com>
2021-06-17 15:25:38 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -5,7 +5,7 @@ A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
 
 Complete o código abaixo para testar as situações em que
-a função recebe como parâmetro true e false, respectivamente.
+a função recebe como parâmetro true e false, respectivamente
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
