b6b6ef7bb6eb6aa79d1904c11a9c74d11c986026
Gabriel Viana <g.viana87@hotmail.com>
2021-06-17 15:41:55 -0300

    linter zagueirao

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,10 +11,8 @@ const answerPhone = require('../src/asyncJest');
 describe('o retorno do telefonema', () => {
   test('atende', () => {
     expect(answerPhone(true)).resolves.toBe('Oi');
-
   });
   test('ocupado', () => {
     expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
-    
   });
 });
