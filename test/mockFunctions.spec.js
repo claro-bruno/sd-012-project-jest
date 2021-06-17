a1e946b698a4579217c68f91836cf8119e3b34df
PeDD1 <pedrohr0909@gmail.com>
2021-06-17 19:27:39 -0300

    Requisito 3 concluido com lint

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -13,9 +13,21 @@ O foco aqui é a utilização de mock functions.
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-
+jest.mock('../src/mockFunctions');
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial.mockImplementation((a) => {
+    let fact = a;
+    for (let index = a - 1; index >= 1; index -= 1) {
+      fact *= index;
+    }
+    return fact;
+  });
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
