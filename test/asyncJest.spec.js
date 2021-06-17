7b38480197f04b945cf0b9676ba6600fa1aab8df
matheustkaczyk <matheus111904@hotmail.com>
2021-06-17 14:32:42 -0300

    Iniciando o projeto!

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,7 +12,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
+    
     // Insira seu teste assíncrono aqui
   });
   test('ocupado', () => {
