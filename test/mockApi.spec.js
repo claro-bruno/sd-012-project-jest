314db8b64d206186b6070572de3ed0bade03a89b
Rodrigo <rodrigo.facury14@gmail.com>
2021-06-17 17:55:01 -0300

    Requisito 3 com lint corrigido

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,25 +23,24 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  api.fetchURL = jest.fn().mockImplementation( async () => {
-      const object = {
-        gender: 'male',
-        name: {
-          first: 'Antônio',
-          last: 'Britto',
-        },
-        location: {
-          country: 'Brazil',
-        },
-        email: 'tunico@bol.com.br',
-        login: {
-          username: 'tunicao123',
-          password: '1234567890',
-        },
-      }
-      return object;
-    }
-  );
+  api.fetchURL = jest.fn().mockImplementation(async () => {
+    const object = {
+      gender: 'male',
+      name: {
+        first: 'Antônio',
+        last: 'Britto',
+      },
+      location: {
+        country: 'Brazil',
+      },
+      email: 'tunico@bol.com.br',
+      login: {
+        username: 'tunicao123',
+        password: '1234567890',
+      },
+    };
+    return object;
+  });
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
