930498e7dd4c2b04a76441f600caa29a2b033d62
Adriana Biberg <adribiberg@hotmail.com>
2021-06-17 17:57:52 -0300

    Cumpre requisito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -21,9 +21,18 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
+const testApi = {
+  gender: 'male',
+  name: { first: 'Antônio', last: 'Britto' },
+  location: { country: 'Brazil' },
+  email: 'tunico@bol.com.br',
+  login: { username: 'tunicao123', password: '1234567890' },
+};
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  api.fetchURL = jest.fn().mockImplementation(async () => testApi);
+  // Usei como referência para o assincronismo a resposta do Ryan Laiber no slack: https://trybecourse.slack.com/archives/C01T2C18DSM/p1623955632184900
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
