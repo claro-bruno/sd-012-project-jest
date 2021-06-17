46e2ceb52a87bb012ffb80965d7041cc4610fce1
Thalles Carneiro <thallescarneiro@gmail.com>
2021-06-17 16:20:35 -0300

    Refatora o Requisito 2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,5 +1,6 @@
 const mockFunctions = require('../src/mockFunctions');
 
+jest.mock('../src/mockFunctions');
 /*
 Criamos uma série de funções com eficiência duvidosa.
 Elas estão no arquivo 'src/mockFunctions.js'.
@@ -16,12 +17,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
-  mockFunctions.add = jest.fn().mockImplementation((a, b) => a + b);
-  mockFunctions.subtract = jest.fn().mockImplementation((a, b) => a - b);
-  mockFunctions.multiply = jest.fn().mockImplementation((a, b) => a * b);
-  mockFunctions.divide = jest.fn().mockImplementation((a, b) => a / b);
-  mockFunctions.power = jest.fn().mockImplementation((a, b) => a ** b);
-  mockFunctions.factorial = jest.fn().mockImplementation((a) => {
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial.mockImplementation((a) => {
     const array = Array.from({ length: a }, (v, k) => k + 1);
     return array.reduce((acc, curr) => acc * curr);
   });
