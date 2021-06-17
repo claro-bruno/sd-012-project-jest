ef1c2518324348a996c4d0b0b93c8522b11c2e7c
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-06-17 16:12:37 -0300

    Resolvendo requisito 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,7 +20,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  adventure.randomAttack();
+  afterEach(() => {
+    adventure.randomAttack();
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
