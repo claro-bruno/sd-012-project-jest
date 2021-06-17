24f594193a30c990c52f08e7d05cd390bb42a235
Álvaro Palma <palma.alvaro@outlook.com>
2021-06-17 18:55:29 -0300

    Ajeita testes do requisito 4 com beforeEach

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,28 +20,24 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  test('depois da primeira aventura', () => {
+  beforeEach(() => {
     adventure.randomAttack();
+    return console.log(adventure.specialists);
+  });
+
+  test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
-    console.log(adventure.specialists);
   });
   test('depois da segunda aventura', () => {
-    adventure.randomAttack();
     expect(adventure.specialists.length).toBe(4);
-    console.log(adventure.specialists);
   });
   test('depois da terceira aventura', () => {
-    adventure.randomAttack();
     expect(adventure.specialists.length).toBe(3);
-    console.log(adventure.specialists);
   });
   test('depois da quarta aventura', () => {
-    adventure.randomAttack();
     expect(adventure.specialists.length).toBe(2);
-    console.log(adventure.specialists);
   });
   test('depois da quinta aventura', () => {
-    adventure.randomAttack();
     expect(adventure.specialists.length).toBe(1);
     console.log(`The last hero up is ${adventure.specialists[0].nome}`);
   });
