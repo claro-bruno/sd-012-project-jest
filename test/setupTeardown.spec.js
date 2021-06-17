567688bcb3b4a2337bbc6afbc4db0e0200fe1277
Teika <shimworks.tk@gmail.com>
2021-06-17 16:46:48 -0300

    resolve requisito 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -1,4 +1,5 @@
 const adventure = require('../src/setupTeardown');
+// jest.mock('../src/setupTeardown');
 /*
 Num universo não tão distante, um grupo de aventureiros da Trybe enfrentam uma série de testes.
 O grupo parte em direção ao sucesso, mas,
@@ -21,6 +22,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
+  beforeEach(() => {
+    console.log(adventure.specialists);
+    adventure.randomAttack();
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
