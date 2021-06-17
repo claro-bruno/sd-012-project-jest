0b5af113defccdd4adde19afe7830023ab22eb66
Marcela Silva <marcela.ops@hotmail.br>
2021-06-17 16:11:52 -0300

    requisito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -21,10 +21,20 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
+const reposFake = {
+
+}
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
-
+  const apiURL = jest.spyOn(api, 'fetchURL');
+  apiURL.mockResolvedValue({
+    gender: 'male',
+    name: { first: 'Antônio', last: 'Britto' },
+    location: { country: 'Brazil'},
+    email: 'tunico@bol.com.br',
+    login: { username: 'tunicao123', password: '1234567890' },
+  });
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
