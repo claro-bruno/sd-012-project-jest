b0ebcc487805caeda4d375f7cec25497193c5f95
Martin <escorpmartin97@gmail.com>
2021-06-17 18:09:04 -0300

    req 2

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  api.fetchURL = jest.fn();
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
