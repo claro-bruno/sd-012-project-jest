8f1996f008403527dce126d00241f6e47563883e
Raphael <raphael.ph2ll@gmail.com>
2021-06-17 16:44:59 -0300

    Resolve ex01 - answerPhone

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,5 @@
-const assert = require('assert');
-// const answerPhone = require('../src/asyncJest');
+// const assert = require('assert');
+const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -11,12 +11,19 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
-  });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('atende', () => answerPhone('oi').then((data) => {
+    expect.assertions(1);
+    // Promisse
+    expect(data).toBe('Oi!');
+  }));
+  test('ocupado', async () => {
+    // Async
+    try {
+      await answerPhone();
+    } catch (error) {
+      expect(error).toEqual(new Error('Infelizmente não podemos atender...'));
+    }
   });
 });
+// Links
+// https://jestjs.io/pt-BR/docs/asynchronous;
--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -1,4 +1,4 @@
-const adventure = require('../src/setupTeardown');
+// const adventure = require('../src/setupTeardown');
 /*
 Num universo não tão distante, um grupo de aventureiros da Trybe enfrentam uma série de testes.
 O grupo parte em direção ao sucesso, mas,
@@ -19,22 +19,22 @@ PS: Os codinomes dos aventureiros são reais! Tentem descobrir quem é quem!
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+// describe('quem sobreviveu?', () => {
+//   // Adicione seu código aqui
 
-  test('depois da primeira aventura', () => {
-    expect(adventure.specialists.length).toBe(5);
-  });
-  test('depois da segunda aventura', () => {
-    expect(adventure.specialists.length).toBe(4);
-  });
-  test('depois da terceira aventura', () => {
-    expect(adventure.specialists.length).toBe(3);
-  });
-  test('depois da quarta aventura', () => {
-    expect(adventure.specialists.length).toBe(2);
-  });
-  test('depois da quinta aventura', () => {
-    expect(adventure.specialists.length).toBe(1);
-  });
-});
+//   test('depois da primeira aventura', () => {
+//     expect(adventure.specialists.length).toBe(5);
+//   });
+//   test('depois da segunda aventura', () => {
+//     expect(adventure.specialists.length).toBe(4);
+//   });
+//   test('depois da terceira aventura', () => {
+//     expect(adventure.specialists.length).toBe(3);
+//   });
+//   test('depois da quarta aventura', () => {
+//     expect(adventure.specialists.length).toBe(2);
+//   });
+//   test('depois da quinta aventura', () => {
+//     expect(adventure.specialists.length).toBe(1);
+//   });
+// });
