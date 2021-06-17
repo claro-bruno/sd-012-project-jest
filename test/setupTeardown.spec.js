c746b5f94f4084c78a393035d16865c51599f326
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-06-17 16:54:15 -0300

    Requisito 4 com lint

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,8 +20,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
-
+  beforeEach(() => {
+    console.log(adventure.specialists);
+    adventure.randomAttack();
+  });
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
