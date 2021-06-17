21c156cdb089b3c73df56f0f325c71a9c223071e
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-06-17 17:23:26 -0300

    Requisito 4 resolvido

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,6 +21,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'!
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.randomAttack();
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
@@ -36,5 +39,7 @@ describe('quem sobreviveu?', () => {
   });
   test('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
+    const [{ nome }] = adventure.specialists;
+    console.log(`O vencedor é ${nome}!`);
   });
 });
