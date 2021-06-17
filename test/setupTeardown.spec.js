a5f5640670b6d1a59ecdee071215b208b7230759
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-06-17 20:44:03 -0300

    requisito 4 feito

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,6 +21,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.specialists.length -= 1;
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
