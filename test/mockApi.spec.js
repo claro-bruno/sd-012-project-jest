947ba3e2c76fb3231d90998cc133c830ce4f6361
Martin <escorpmartin97@gmail.com>
2021-06-17 18:59:14 -0300

    req 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,25 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  api.fetchURL = jest.fn();
+  api.fetchURL = jest.fn().mockResolvedValue({
+    gender: 'male',
+
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+
+    location: {
+      country: 'Brazil',
+    },
+
+    email: 'tunico@bol.com.br',
+
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  });
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
