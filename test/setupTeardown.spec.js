74e5035960b75e499f0131fae27a8f54096a6ee0
Merlone <alestori7@gmail.com>
2021-06-17 11:15:02 -0300

    commit inicial - npm install

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -18,7 +18,6 @@ PS: Os codinomes dos aventureiros são reais! Tentem descobrir quem é quem!
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
 
