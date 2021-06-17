2131325f19415fe919a9e80dd2fe695bc761b239
Merlone <alestori7@gmail.com>
2021-06-17 14:05:11 -0300

    adiciona intellisense pro jest - começo do requisito 2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -13,9 +13,14 @@ O foco aqui é a utilização de mock functions.
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
+jest.mock('../src/mockFunctions');
 
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
