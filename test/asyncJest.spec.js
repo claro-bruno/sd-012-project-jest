9291d100e0d79cb3ebd6e932b0716c9c7671b143
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-06-17 15:40:58 -0300

    feito requisito 1 utilizando formas variadas de teste

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -9,12 +9,39 @@ a função recebe como parâmetro true e false, respectivamente.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
+const errorMessage = 'Infelizmente não podemos atender...';
+
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
+  test('atende, testado com resolves', () => {
     expect(answerPhone(true)).resolves.toBe('Oi!');
   });
 
-  test('ocupado', () => {
-    expect(answerPhone(false)).rejects.toThrow({ message: 'Infelizmente não podemos atender...' });
+  test('ocupado, testado com rejects', () => {
+    expect(answerPhone(false)).rejects.toThrow({ message: errorMessage });
+  });
+
+  test('atende, testado com then', () => {
+    expect.assertions(1);
+    return answerPhone(true).then((response) => expect(response).toBe('Oi!'));
+  });
+
+  test('ocupado, testado com catch', () => {
+    expect.assertions(1);
+    return answerPhone(false).catch((error) => expect(error.message).toBe(errorMessage));
+  });
+
+  test('atende, testado com done', async (done) => {
+    const response = await answerPhone(true);
+    expect(response).toBe('Oi!');
+    done();
+  });
+
+  test('ocupado, testado com done', async (done) => {
+    try {
+      await answerPhone(false);
+    } catch (error) {
+      expect(error.message).toBe(errorMessage);
+      done();
+    }
   });
 });
