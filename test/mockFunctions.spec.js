e2d49417a2123a63bd3352cbc818c93f0bd9386f
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-06-17 15:13:47 -0300

    requirement 2 done, sync function with mock

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,5 +1,5 @@
 const mockFunctions = require('../src/mockFunctions');
-
+jest.mock('../src/mockFunctions');
 /*
 Criamos uma série de funções com eficiência duvidosa.
 Elas estão no arquivo 'src/mockFunctions.js'.
@@ -15,7 +15,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.power.mockImplementation((a, b) => Math.pow(a, b));
+  const factorial = (num) => num === 1 ? num : factorial(num - 1) * num;
+  mockFunctions.factorial.mockImplementation(factorial);
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
