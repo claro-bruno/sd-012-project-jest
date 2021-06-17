40f9417dab59cde069a9c65f79d39729ab0cfa68
Carlos Eduardo Tomé da Silva <caduts10@gmail.com>
2021-06-17 20:09:16 -0300

    requisito 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,7 +20,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => adventure.randomAttack());
+  afterEach(() => console.log(adventure.specialists));
+  afterAll(() => console.log(adventure.specialists[0].nome));
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
