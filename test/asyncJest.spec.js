c9e1aa57c839aa251584b5e6063b718df87bab43
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-06-17 15:47:16 -0300

    Requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,5 @@
-const assert = require('assert');
-// const answerPhone = require('../src/asyncJest');
+// const assert = require('assert');
+const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -12,11 +12,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    // const waitingAnswer = await answerPhone(true); (com async)
+    // console.log(waitingAnswer); se eu coloco o console.log antes recebo "Promise { Oi! }". quando coloco aqui dentro, recebo só "Oi!" pq? pq a promise foi resolvida depois do await
+    expect(answerPhone(true)).resolves.toBe('Oi!');
   });
   test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    // try {
+    //  await answerPhone(false);
+    // } catch (error) {
+    //   expect(error).toEqual(new Error('Infelizmente não podemos atender...'));
+    // };
+    expect(answerPhone(false)).rejects.toEqual(new Error('Infelizmente não podemos atender...'));
   });
 });
