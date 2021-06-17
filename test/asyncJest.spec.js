627abab96674710fad752ce5f6428705dbc2e356
matheus oliveira de proenca <mproenca.marketing@gmail.com>
2021-06-17 15:52:46 -0300

    adiciona test 01

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
-// const answerPhone = require('../src/asyncJest');
+const { hasUncaughtExceptionCaptureCallback } = require('process');
+const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -12,11 +13,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    // assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(true)
+      .then(data => expect(data).toBe('Oi!'));
   });
   test('ocupado', () => {
-    // assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone('ocupado')
+      .catch(error => expect(error).toEqual(Error('Infelizmente não podemos atender...')))
   });
 });
