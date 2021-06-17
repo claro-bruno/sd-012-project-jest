b0c4bee1e772f51eea460caa5892b405948dfcd8
Augusto <augustocm2002@yahoo.com.br>
2021-06-17 14:30:16 -0300

    Iniciando projeto!

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -9,7 +9,7 @@ a função recebe como parâmetro true e false, respectivamente.
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-
+// Iniciando projeto!
 describe('o retorno do telefonema', () => {
   test('atende', () => {
     assert.fail();
