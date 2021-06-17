146039434cb39be1908f7ef744f17a5e08cc42c3
Eduardo Linhares <modsgtasagame@gmail.com>
2021-06-17 17:08:09 -0300

    Resolve requisito 2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,4 +1,5 @@
 const mockFunctions = require('../src/mockFunctions');
+jest.mock('../src/mockFunctions');
 
 /*
 Criamos uma série de funções com eficiência duvidosa.
@@ -16,6 +17,21 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
+
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial.mockImplementation((a) => {
+    if (a === 0 || a === 1) {
+      return 1;
+    }
+    for (let i = a - 1; i >= 1; i -= 1) {
+      a *= i;
+    }
+    return a;
+  })
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
