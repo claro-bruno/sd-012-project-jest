ab9a1d5644f8169d6a584e8d87cff6eb3f54828a
cristianocsl <cslcristiano@gmail.com>
2021-06-17 17:41:06 -0300

    Requisito 3: concluido com sucesso! :rocket:

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,9 +24,18 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const mockFunct = jest
+    .spyOn(api, 'fetchURL')
+    .mockImplementation(async () => ({
+      gender: 'male',
+      name: { first: 'Antônio', last: 'Britto' },
+      location: { country: 'Brazil' },
+      email: 'tunico@bol.com.br',
+      login: { username: 'tunicao123', password: '1234567890' },
+    }));
 
   test('verifica se o usuário é o tunico', async () => (
-    api.fetchURL().then((user) => {
+    mockFunct().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
       expect(user.name.last).toEqual('Britto');
