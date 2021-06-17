32ec989dcdffde84c9706ccbd04719975d99e8cf
Eicke Yumoto <eickeyumoto@gmail.com>
2021-06-17 14:26:46 -0300

    :rocket:

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -9,7 +9,6 @@ a função recebe como parâmetro true e false, respectivamente.
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-
 describe('o retorno do telefonema', () => {
   test('atende', () => {
     assert.fail();
