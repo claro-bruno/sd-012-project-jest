f5f919bcb03aa9f2788f1e28e58e9326900366a5
tiagoornelas <advtiagoornelas@gmail.com>
2021-06-18 11:10:07 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/asyncJest.js
+++ src/asyncJest.js
@@ -1,8 +1,11 @@
-// ATENÇÃO!!! Não edite este arquivo. Altere apenas os arquivos da pasta 'test'.
+// ATENÇÃO!!! Não edite este arquivo. Altere apenas os arquivos da pasta 'test'!
 const answerPhone = (answer) =>
   new Promise(function (resolve, reject) {
     setTimeout(
-      answer ? resolve('Oi!') : reject(new Error('Infelizmente não podemos atender...')), 3000,
+      answer
+        ? resolve('Oi!')
+        : reject(new Error('Infelizmente não podemos atender...')),
+      3000
     );
   });
 
