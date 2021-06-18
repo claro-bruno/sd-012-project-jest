9f7fed9371b7297849b4c38b7f226d196769af83
Samuel Constantino <samuelicapuidados@gmail.com>
2021-06-18 14:48:01 -0300

    testa requisito 1 de várias formas

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -13,14 +13,37 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
+  test('atende, testado com resolves', () => {
     // assert.fail();
     // Insira seu teste assíncrono aqui
     expect(answerPhone(true)).resolves.toBe('Oi!');
   });
-  test('ocupado', async () => {
-    // assert.fail();
-    // Insira seu teste assíncrono aqui
-    expect(answerPhone(false)).rejects.toThrow(new Error(ERROR_MESSAGE));
+  test('ocupado, testado com rejects', () => {
+    expect(answerPhone(false)).rejects.toThrow({ message: ERROR_MESSAGE });
+  });
+
+  test('atende, testado com then', () => {
+    expect.assertions(1);
+    return answerPhone(true).then((response) => expect(response).toBe('Oi!'));
+  });
+
+  test('ocupado, testado com catch', () => {
+    expect.assertions(1);
+    return answerPhone(false).catch((error) => expect(error.message).toBe(ERROR_MESSAGE));
+  });
+
+  test('atende, testado com done', async (done) => {
+    const response = await answerPhone(true);
+    expect(response).toBe('Oi!');
+    done();
+  });
+
+  test('ocupado, testado com done', async (done) => {
+    try {
+      await answerPhone(false);
+    } catch (error) {
+      expect(error.message).toBe(ERROR_MESSAGE);
+      done();
+    }
   });
 });
