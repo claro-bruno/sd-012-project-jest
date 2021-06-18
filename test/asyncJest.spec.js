205b19a46a83f0145d4ab2224288f34905bfbced
Josimar Souza <contact.josimarsouza@gmail.com>
2021-06-18 14:47:13 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
+  test('atende', () => {    
     // Insira seu teste assíncrono aqui
   });
-  test('ocupado', () => {
-    assert.fail();
+  test('ocupado', () => {    
     // Insira seu teste assíncrono aqui
   });
 });
