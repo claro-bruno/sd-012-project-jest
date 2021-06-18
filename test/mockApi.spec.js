d2b0857d12ceec7bd5a76610d62604d57105a8a0
Eder Santos <3derms@gmail.com>
2021-06-18 15:53:26 -0300

    corrige falso positivo em mockApi

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,14 +23,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 // jest.mock('../src/mockApi');
 describe('verifica o usuário', () => {
-  test('verifica se o usuário é o Tunico', () => {
-    api.fetchURL = jest.fn().mockImplementation(async () => ({
-      gender: 'male',
-      name: { first: 'Antônio', last: 'Britto' },
-      location: { country: 'Brazil' },
-      email: 'tunico@bol.com.br',
-      login: { username: 'tunicao123', password: '1234567890' },
-    }));
+  api.fetchURL = jest.fn().mockImplementation(async () => ({
+    gender: 'male',
+    name: { first: 'Antônio', last: 'Britto' },
+    location: { country: 'Brazil' },
+    email: 'tunico@bol.com.br',
+    login: { username: 'tunicao123', password: '1234567890' },
+  }));
+  test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
@@ -39,6 +39,6 @@ describe('verifica o usuário', () => {
       expect(user.email).toEqual('tunico@bol.com.br');
       expect(user.login.username).toEqual('tunicao123');
       expect(user.login.password).toEqual('1234567890');
-    });
-  });
+    })
+  ));
 });
