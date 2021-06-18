024f164833ea617aaacd4d4745cd2889a69c5c2b
Raphael <raphael.ph2ll@gmail.com>
2021-06-18 13:19:27 -0300

    Resolve ex03 - fakeApi

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -1,4 +1,4 @@
-// const api = require('../src/mockApi');
+const api = require('../src/mockApi');
 
 /*
 A função fetchURL retorna um JSON com informações de um usuário aleatório buscadas da API 'randomuser.me'.
@@ -22,18 +22,38 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-// describe('verifica o usuário', () => {
-//   // Crie sua mock da função fetchURL() aqui
+describe('verifica o usuário', () => {
+  // Crie sua mock da função fetchURL() aqui
+  const fakeApi = {
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
+  api.fetchURL = jest.fn()
+    .mockImplementation(async () => fakeApi);
+  test('verifica se o usuário é o tunico', async () => (
+    api.fetchURL().then((user) => {
+      expect(user.gender).toEqual('male');
+      expect(user.name.first).toEqual('Antônio');
+      expect(user.name.last).toEqual('Britto');
+      expect(user.location.country).toEqual('Brazil');
+      expect(user.email).toEqual('tunico@bol.com.br');
+      expect(user.login.username).toEqual('tunicao123');
+      expect(user.login.password).toEqual('1234567890');
+    })
+  ));
+});
+// Links
+// Segui a recomendação do Igor Fernandes repo: https://github.com/tryber/sd-012-project-jest/blob/Igor-Fernandes-project-Jest/test/mockApi.spec.js
 
-//   test('verifica se o usuário é o tunico', async () => (
-//     api.fetchURL().then((user) => {
-//       expect(user.gender).toEqual('male');
-//       expect(user.name.first).toEqual('Antônio');
-//       expect(user.name.last).toEqual('Britto');
-//       expect(user.location.country).toEqual('Brazil');
-//       expect(user.email).toEqual('tunico@bol.com.br');
-//       expect(user.login.username).toEqual('tunicao123');
-//       expect(user.login.password).toEqual('1234567890');
-//     })
-//   ));
-// });
+//  https://app.betrybe.com/course/fundamentals/testes-automatizados-com-jest/jest-simulando-comportamentos/4783a3a1-485b-4712-a293-b645c2f7ac40/conteudos/2ef333ad-385a-408f-8b3f-c6ce0ebf9a49/mock-e-funcoes-assincronas/1a98003e-a718-414d-a9ff-9910a1aaf0d6?use_case=side_bar
