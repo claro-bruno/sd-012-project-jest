400f0c1851ed1ab775c72f35400d91481c9f68a1
ViniciusMossi <vini_agg@hotmail.com>
2021-06-18 17:08:04 -0400

    inicia projeto

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -14,6 +14,7 @@ describe('o retorno do telefonema', () => {
   test('atende', () => {
     assert.fail();
     // Insira seu teste assíncrono aqui
+    teste
   });
   test('ocupado', () => {
     assert.fail();
