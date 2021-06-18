bc709c8a9e52b27d7ea8ad412c1111033385c8d7
Henrique Remedios <henriqueremedios@gmail.com>
2021-06-18 17:03:29 -0300

    3 - Cria um Mock para o retorno da API

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,21 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  api.fetchURL = jest.fn().mockImplementation(async () => ({
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  }));
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
