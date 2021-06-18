42a49d2b1763ecc3ca423e689c5c2344865a1067
Samuel Constantino <samuelicapuidados@gmail.com>
2021-06-18 16:16:29 -0300

    refatora requisito 3: troca spyOn por jest.fn

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,18 +24,22 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
-  const obj = {
+  const fakeResponse = {
     gender: 'male',
-    name: { first: 'Antônio', last: 'Britto' },
-    location: { country: 'Brazil' },
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
     email: 'tunico@bol.com.br',
     login: {
       username: 'tunicao123',
       password: '1234567890',
     },
   };
-
-  jest.spyOn(api, 'fetchURL').mockResolvedValue(obj);
+  api.fetchURL = jest.fn().mockResolvedValue(fakeResponse);
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
