16d6a1f0496c63a236704ab54bd1806f20115d92
Janderson Soares <jandinsm@gmail.com>
2021-06-18 20:44:27 -0300

    Desafio 4, beforeEach

--- src/setupTeardown.js
+++ src/setupTeardown.js
@@ -23,5 +23,5 @@ const randomAttack = () => {
   console.log(`Rest in peace, ${nome}...`);
   specialists.splice(randomSpecialistIndex, 1);
 };
-randomAttack();
+
 module.exports = { specialists, randomAttack };
