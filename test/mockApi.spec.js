ae01872c5c970f56da6b59a3fc10c6513bff66a0
Rafael Cardoso <rafael.vimur@gmail.com>
2021-06-18 14:27:09 -0300

    adiciona requisito 3 e correçoes de lint requisito 1 e 2

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  const info = {
+    gender: 'male',
+    name: { title: 'Mr', first: 'Antônio', last: 'Britto' },
+    location: { country: 'Brazil' },
+    email: 'tunico@bol.com.br',
+    login: { username: 'tunicao123', password: '1234567890' },
+  };
+
+  api.fetchURL = jest.fn().mockResolvedValue(info);
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -56,8 +56,8 @@ describe('verifica as funções e os mocks', () => {
     expect(mockFunctions.divide(1331, 11)).toEqual(121);
   });
   test('testa função power', () => {
-    mockFunctions.power.mockImplementation((a, b) => a ** b)
-    
+    mockFunctions.power.mockImplementation((a, b) => a ** b);
+
     expect(mockFunctions.power(10, 2)).toEqual(100);
     expect(mockFunctions.power(2, 10)).toEqual(1024);
     expect(mockFunctions.power(5, 5)).toEqual(3125);
