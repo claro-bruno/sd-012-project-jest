3e54a4f2ce8ae1f5fce30ba8f5ecf4d2f75aeb26
Sarah <sarahgsb@hotmail.com>
2021-06-18 05:08:26 -0300

    correcao lint requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,10 +11,6 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    return expect(answerPhone(true)).resolves.toBe('Oi!');
-  });
-  test('ocupado', () => {
-    return expect(answerPhone(false)).rejects.toEqual((new Error('Infelizmente não podemos atender...')));
-    });
-});s
+  test('atende', () => expect(answerPhone(true)).resolves.toBe('Oi!'));
+  test('ocupado', () => expect(answerPhone(false)).rejects.toEqual((new Error('Infelizmente não podemos atender...'))));
+});
