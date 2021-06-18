a61038a323fbb10c8b29dd8e1437efbf5c397c06
Sarah <sarahgsb@hotmail.com>
2021-06-18 05:16:00 -0300

    corrige identacao requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -13,5 +13,5 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('o retorno do telefonema', () => {
   test('atende', () => expect(answerPhone(true)).resolves.toBe('Oi!'));
   test('ocupado', () => expect(answerPhone(false))
-  .rejects.toEqual((new Error('Infelizmente não podemos atender...'))));
+    .rejects.toEqual((new Error('Infelizmente não podemos atender...'))));
 });
