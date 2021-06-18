22f516f2396891123da9871cec2deffee9a8a440
rachidb5 <jordan.rachid@gmail.com>
2021-06-18 14:46:11 -0300

    Teste 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,9 +24,22 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const user = {
+    gender: 'male',
+    name: { first: 'Antônio', last: 'Britto' },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  }
+  api.fetchURL = jest.fn().mockImplementation(user);
 
   test('verifica se o usuário é o tunico', async () => (
-    api.fetchURL().then((user) => {
+    api.fetchURL = () => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
       expect(user.name.last).toEqual('Britto');
@@ -35,5 +48,5 @@ describe('verifica o usuário', () => {
       expect(user.login.username).toEqual('tunicao123');
       expect(user.login.password).toEqual('1234567890');
     })
-  ));
-});
+  );
+})
