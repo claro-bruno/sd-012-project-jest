fdda10944f7da9ab9082722b05dd2bc407da9475
Camila Damásio <camiladamasioef@hotmail.com>
2021-06-18 18:33:31 -0300

    Requisito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -21,9 +21,24 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-
+const dataTunico = {
+  gender: 'male',
+  name: {
+    first: 'Antônio',
+    last: 'Britto',
+  },
+  location: {
+    country: 'Brazil',
+  },
+  email: 'tunico@bol.com.br',
+  login: {
+    username: 'tunicao123',
+    password: '1234567890',
+  },
+};
+// Requisito resolvido na mentoria da Gisele com a ajuda da própria Gisele e do colega Roberval
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  api.fetchURL = jest.fn().mockResolvedValue(dataTunico);
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
