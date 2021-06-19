7456363bd247a668e0861615cb8203c84b6514fe
Wadson <wadson.dev92@gmail.com>
2021-06-19 13:06:47 -0300

    Novo commit com correçao de linter.  Resolvido as testes das funçoes asyncJest e mockFunctions, com a ajuda do Roberval no multirao da turma 12 de hoje(19/06)

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -13,19 +13,19 @@ O foco aqui é a utilização de mock functions.
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-jest.mock('../src/mockFunctions')
+jest.mock('../src/mockFunctions');
 
 describe('verifica as funções e os mocks', () => {
-  mockFunctions.add.mockImplementation((a, b) => a + b)
-  mockFunctions.subtract.mockImplementation((a, b) => a - b)
-  mockFunctions.multiply.mockImplementation((a, b) => a * b)
-  mockFunctions.divide.mockImplementation((a, b) => a / b)
-  mockFunctions.power.mockImplementation((a, b) => a ** b)
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
   mockFunctions.factorial.mockImplementation((a) => {
     let fact = 1;
     for (let i = 2; i <= a; i += 1) fact *= i;
     return fact;
-  })
+  });
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
