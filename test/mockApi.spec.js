117f662b8cf9a7e39189e5b6a6c626d57a623612
Mateus <mateusbgonzaga@gmail.com>
2021-06-20 17:59:07 -0300

    second task v1

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,10 +23,19 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  
 
-  test('verifica se o usuário é o tunico', async () => (
-    api.fetchURL().then((user) => {
+  const tunicao = {
+    gender: 'male',
+    name: { first: 'Antônio', last: 'Britto' },
+    location: { country: 'Brazil' },
+    email: 'tunico@bol.com.br',
+    login: { username: 'tunicao123', password: '1234567890' },
+  };
+
+  test('verifica se o usuário é o tunico', async () => {
+    api.fetchURL = jest.fn().mockResolvedValue(tunicao);
+    return api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
       expect(user.name.last).toEqual('Britto');
@@ -34,6 +43,6 @@ describe('verifica o usuário', () => {
       expect(user.email).toEqual('tunico@bol.com.br');
       expect(user.login.username).toEqual('tunicao123');
       expect(user.login.password).toEqual('1234567890');
-    })
-  ));
+    });
+  });
 });
