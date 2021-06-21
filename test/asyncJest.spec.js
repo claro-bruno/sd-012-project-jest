2d3ccbd20ada30a1be260e4fa4cf13c32b1735f6
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-06-21 12:45:16 -0300

    iniciando o projeto

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
 
-Complete o código abaixo para testar as situações em que
+Complete o código abaixo para testar as situações em que 
 a função recebe como parâmetro true e false, respectivamente.
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
