d8bc434cf2dee66894f0a66d7445775da9a43e68
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-06-22 09:44:30 -0300

    adiciona requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,4 @@
-const assert = require('assert');
-// const answerPhone = require('../src/asyncJest');
+const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -11,12 +10,7 @@ ATENÇÃO!!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
-  });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
-  });
-});
+  test('atende', async () => expect(answerPhone(true)).resolves.toBe('Oi!'));
+
+  test('ocupado', async () => expect(answerPhone(false)).rejects.toEqual(new Error ('Infelizmente não podemos atender...'))
+)});
