31d22931ece0d54807c3da27bfc496ea87ee47ed
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-06-22 11:39:05 -0300

    adiciona requisito 3 lint

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -25,19 +25,19 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('verifica o usuário', () => {
   api.fetchURL = jest.fn(async function teste() {
     return ({
-    gender: 'male',
-    name: {
-      first: 'Antônio', last: 'Britto'
-    },
-    location: {
-      country: 'Brazil'
-    },
-    email: 'tunico@bol.com.br',
-    login: {
-      username: 'tunicao123',
-      password: '1234567890'
-    },
-  });
+      gender: 'male',
+      name: {
+        first: 'Antônio', last: 'Britto',
+      },
+      location: {
+        country: 'Brazil',
+      },
+      email: 'tunico@bol.com.br',
+      login: {
+        username: 'tunicao123',
+        password: '1234567890',
+      },
+    });
   });
 
   test('verifica se o usuário é o tunico', () => (
