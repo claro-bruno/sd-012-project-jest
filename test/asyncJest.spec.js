6a3dd9e7ca93a56677a389fc8cda12da9e3dc5e3
martinsph <ph.mmartins@gmail.com>
2021-06-21 23:02:31 -0300

    test req 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('atende', async () => {
+    expect.assertions(1);
+
+    const result = await answerPhone(true);
+    return expect(result).resolves.toBe('Oi!');
+    
   });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado', async () => {
+    expect.assertions(1);
+
+    const result = await answerPhone(false);
+    return expect(result).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
