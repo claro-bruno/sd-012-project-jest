ec08bf7282a02973ed480218b7a1f703f3f6f8a0
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-06-25 09:56:25 -0300

    Requisito 4.Criando funções no arquivo test/setupTeardown.spec.js

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,6 +21,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.randomAttack();
+    console.log(adventure.specialists);
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
