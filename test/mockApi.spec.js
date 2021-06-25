33879788fbeef8b5245132794c594218553760d2
Rodrigo Grande <rogrande0496@gmail.com>
2021-06-25 02:15:11 -0300

    completa requisito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,21 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  const userInfo = ({
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: { country: 'Brazil' },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  });
+
+  jest.spyOn(api, 'fetchURL').mockResolvedValue(userInfo);
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
