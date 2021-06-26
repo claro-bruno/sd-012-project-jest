5333529fc25ec40a831e8f212315d06b89de834f
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-06-26 13:27:14 -0300

    requisito 3 - implementa fakeUser e mock a funcao mockApi resolvido

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -1,5 +1,13 @@
 const api = require('../src/mockApi');
 
+const user = {
+  gender: 'male',
+  name: { first: 'Antônio', last: 'Britto' },
+  location: { country: 'Brazil' },
+  email: 'tunico@bol.com.br',
+  login: { username: 'tunicao123', password: '1234567890' },
+};
+
 /*
 A função fetchURL retorna um JSON com informações de um usuário aleatório buscadas da API 'randomuser.me'.
 No entanto, nos testes abaixo, queremos que todas as vezes que chamarmos a API a resposta contenha as informações do nosso adminis..Cof! Cof!.. programador favorito, Tunicão.
@@ -24,6 +32,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  api.fetchURL = jest
+  .fn()
+  .mockImplementation(async () => user)
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
