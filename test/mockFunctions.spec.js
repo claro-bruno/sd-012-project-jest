af4c773ef44c7adae041d2eb5cc0aca9b3918c64
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-06-26 12:54:35 -0300

    requisito 2 - mocka a funcao inteira e redefine saidas resolvido

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,5 +1,5 @@
 const mockFunctions = require('../src/mockFunctions');
-
+jest.mock('../src/mockFunctions');
 /*
 Criamos uma série de funções com eficiência duvidosa.
 Elas estão no arquivo 'src/mockFunctions.js'.
@@ -16,6 +16,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
+  mockFunctions.add = (a, b) => a + b;
+  mockFunctions.subtract = ((a, b) => a - b);
+  mockFunctions.divide = ((a, b) => a / b);
+  mockFunctions.multiply = ((a, b) => a * b);
+  mockFunctions.power = ((a, b) => a ** b);
+  mockFunctions.factorial = ((a) => a > 0 ? a * mockFunctions.factorial(a-1) : 1 );
+
+
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
