7aab133d0dd5cd0a809111452e7a29969a0627c0
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-06-27 10:10:08 -0300

    requisito 2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -17,6 +17,31 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
 
+  mockFunctions.add = jest.fn();
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+
+  mockFunctions.subtract = jest.fn();
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+
+  mockFunctions.multiply = jest.fn();
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+
+  mockFunctions.divide = jest.fn();
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+
+  mockFunctions.power = jest.fn();
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+
+  // Referência: https://serprogramador.com.br/artigos/topico/javascript/Como-calcular-a-operacao-matematica-de-fatorial-com-JavaScript
+  mockFunctions.factorial = jest.fn();
+  mockFunctions.factorial.mockImplementation((a) => {
+    let resultado = 1;
+    for (let i = 1; i <= a; i += 1) {
+      resultado *= i;
+    }
+    return resultado;
+  });
+
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
