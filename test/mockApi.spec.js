f37c494857ebd6ea2f92ca76f19f3792d0433ca7
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-06-29 20:40:38 -0300

    resolve requesito 3 e o lint do mesmo

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -34,18 +34,18 @@ const admin = {
     coordinates: { latitude: '26.2451', longitude: '45.2995' },
     timezone: {
       offset: '+5:30',
-      description: 'Bombay, Calcutta, Madras, New Delhi'
-    }
+      description: 'Bombay, Calcutta, Madras, New Delhi',
+    },
   },
   email: 'tunico@bol.com.br',
   login: {
     uuid: '45db2b1f-1c9a-4a80-9572-e46614f86c30',
-    username: 'tunico123',
+    username: 'tunicao123',
     password: '1234567890',
     salt: 'XKOOGc2x',
     md5: '8cb7b4686f3869247b3ed189de780ea6',
     sha1: 'c24641f415cf36f4494ea4007fb3d77b47a6aad5',
-    sha256: 'a7bdd079ead0adf21f30cee5b94e5581a9fa0d5fc8b3c1881dbc864dabc55a80'
+    sha256: 'a7bdd079ead0adf21f30cee5b94e5581a9fa0d5fc8b3c1881dbc864dabc55a80',
   },
   dob: { date: '1965-10-01T06:35:49.694Z', age: 55 },
   registered: { date: '2009-02-11T05:48:39.772Z', age: 11 },
@@ -55,13 +55,13 @@ const admin = {
   picture: {
     large: 'https://randomuser.me/api/portraits/women/7.jpg',
     medium: 'https://randomuser.me/api/portraits/med/women/7.jpg',
-    thumbnail: 'https://randomuser.me/api/portraits/thumb/women/7.jpg'
+    thumbnail: 'https://randomuser.me/api/portraits/thumb/women/7.jpg',
   },
-  nat: 'IE'
-}
+  nat: 'IE',
+};
 
 describe('verifica o usuário', () => {
-  api.fetchURL = jest.fn().mockReturnValue(admin)
+  api.fetchURL = jest.fn().mockResolvedValue(admin);
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
