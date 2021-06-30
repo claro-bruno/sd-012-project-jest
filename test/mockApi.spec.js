665e293f0b1f7540684fd6c84fd399738dc7622a
Alice Gonçalves <galice9992@gmail.com>
2021-06-30 15:22:59 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,7 +24,6 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
-
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
