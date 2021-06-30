aa6db93be6a194a9237d92e8a7d61c91eb269e18
Gutemberg <gttj2010@hotmail.com>
2021-06-30 11:06:07 -0300

    Requisito 2 (Inverti a ordem do Reuisito 1 com o 2. Este na verdaade e o 1): Refatoraçao 1 do AsyncJest

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,5 @@
-const assert = require('assert');
-// const answerPhone = require('../src/asyncJest');
+// const assert = require('assert');
+const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -12,7 +12,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    //assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
     expect.assertions(1);
     return answerPhone('alô').then((answer) => {
@@ -20,7 +20,7 @@ describe('o retorno do telefonema', () => {
     });
   });
   test('ocupado', () => {
-    //assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
     expect.assertions(1);
     return answerPhone().catch((answer) => {
