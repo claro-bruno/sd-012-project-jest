f403822cb381c02630f78528aabc464d36c47226
Ana Laura <analauraalves25@gmail.com>
2021-07-01 15:30:34 -0300

    testes da funçao setupTeardown ok

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,7 +20,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => adventure.randomAttack());
+  afterEach(() => console.log(adventure.specialists));
+  afterAll(() => console.log(adventure.specialists.map((spe) => `O sobrevivente foi ${spe}`)));
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
