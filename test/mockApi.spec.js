d8c5417af63f6fd99297e0476abc55b07a4bb4f7
Alice Gonçalves <galice9992@gmail.com>
2021-07-01 18:13:31 -0300

    resolve requisito 3, mockar funçao com requisiçao a uma API, para passar nos testes previamente implementados

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -21,9 +21,20 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-
+jest.mock('../src/mockApi');
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  api.fetchURL.mockImplementation(() => {
+    const user = new Promise(function (resolve) {
+      resolve({
+        gender: 'male',
+        name: { first: 'Antônio', last: 'Britto' },
+        location: { country: 'Brazil' },
+        email: 'tunico@bol.com.br',
+        login: { username: 'tunicao123', password: '1234567890' },
+      });
+    });
+    return user;
+  });
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
