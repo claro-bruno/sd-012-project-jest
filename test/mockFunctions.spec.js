22fcb78fe0acfcfbabe40e4bca41367778ff73b3
Stefano <stefanooliveira1@gmail.com>
2021-07-05 01:02:02 -0300

    Req 2 - implements mock tests

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -16,6 +16,18 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial.mockImplementation((a) => {
+    let num = 1;
+    for (let counter = a; i > 0; counter -= 1) {
+      num *= counter;
+    }
+    return num;
+  });
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
