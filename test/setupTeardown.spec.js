99093685110d45ceee5be430375de0b2cac1d689
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-07-05 20:10:56 -0300

    Adiciona requisito 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,19 +21,23 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
-
+  adventure.randomAttack();
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
+  adventure.randomAttack();
   test('depois da segunda aventura', () => {
     expect(adventure.specialists.length).toBe(4);
   });
+  adventure.randomAttack();
   test('depois da terceira aventura', () => {
     expect(adventure.specialists.length).toBe(3);
   });
+  adventure.randomAttack();
   test('depois da quarta aventura', () => {
     expect(adventure.specialists.length).toBe(2);
   });
+  adventure.randomAttack();
   test('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
   });
