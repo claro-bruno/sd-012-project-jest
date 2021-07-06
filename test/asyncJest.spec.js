772e47f076aeb7795da0d1e93a81e3c5a10d96f5
Caio-Veloso <caio.veloso@gmail.com>
2021-07-06 11:58:26 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 // const answerPhone = require('../src/asyncJest');
 /*
-A função answerPhone recebe um parâmetro boleano.
+A função answerPhon recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
 
 Complete o código abaixo para testar as situações em que
