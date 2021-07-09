9d0e8746cef863ce7973fafeef701352ba21c0fd
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-09 01:40:57 -0400

    Desafio 3 mocka api

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -21,9 +21,24 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
+// recebi ajuda do meu amigo kelvin Wevertor https://github.com/KelvinWevertor
+
+const date = {
+  gender: 'male',
+  name: { first: 'Antônio', last: 'Britto' },
+  location: {
+    country: 'Brazil',
+  },
+  email: 'tunico@bol.com.br',
+  login: {
+    username: 'tunicao123',
+    password: '1234567890',
+  },
+};
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  api.fetchURL = jest.fn().mockResolvedValue(date);
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
