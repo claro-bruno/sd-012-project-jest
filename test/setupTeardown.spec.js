f4bb4c7979c73d5520d7bd58e7dfc24cd35a4887
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-07-09 01:58:30 -0400

    Desafio 4 configura beforeEach e afterEach da funçao setupTeardown

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,6 +21,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
+  beforeEach(() => adventure.randomAttack());
+
+  afterEach(() => console.log(adventure.specialists));
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
